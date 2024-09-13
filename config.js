836b242c8dbb063cd3d89468397cb1fb080aa1ab
Nexus-dev344 <stephenkioni219@gmail.com>
2024-09-13 19:23:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -23,8 +23,8 @@ global.location = '1' //Ganti Pake Location Panel Lu
 //===========================//
 
 global.prefa = ['','!','.',',','🐤','🗿']
-global.owner = ['2349013517834']
-global.ownMain = '2349013517834'
+global.owner = ['254768801963']
+global.ownMain = '254768801963'
 global.NamaOwner = '𝕶𝖎𝖓𝖌 𝕾𝖆𝖒' //gausah diganti 
 global.sessionName = 'spidersession'
 global.connect = true // Ubah Ke False Jika Ingin Menggunakan Qr Code
